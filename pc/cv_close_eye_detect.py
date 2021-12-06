2f4355497bb422b0758c557149a7948d857e27e6
Seima-Yonesho <bule.impulse1201@gmail.com>
2021-12-06 21:42:07 +0900

    fix cv close eye detect

--- pc/cv_close_eye_detect.py
+++ pc/cv_close_eye_detect.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 import cv2
+import time
 eye_cascPath = '../haarcascade_eye_tree_eyeglasses.xml'  #eye detect model
 face_cascPath = '../haarcascade_frontalface_alt.xml'  #face detect model
 faceCascade = cv2.CascadeClassifier(face_cascPath)
@@ -8,44 +10,53 @@ eyeCascade = cv2.CascadeClassifier(eye_cascPath)
 
 
 cap = cv2.VideoCapture(0)
-save_path = "./images/"
+save_path = "../images/"
+cnt = 0
 num = 1
 
 while True:
-    ret, img = cap.read()
-    if ret:
-        frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # Detect faces in the image
-        faces = faceCascade.detectMultiScale(
-            frame,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            # flags = cv2.CV_HAAR_SCALE_IMAGE
+  ret, img = cap.read()
+  if ret:
+    frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+      frame,
+      scaleFactor=1.1,
+      minNeighbors=5,
+      minSize=(30, 30),
+      # flags = cv2.CV_HAAR_SCALE_IMAGE
+    )
+    # print("Found {0} faces!".format(len(faces)))
+    if len(faces) > 0:
+      # Draw a rectangle around the faces
+      for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        frame_tmp = img[faces[0][1]:faces[0][1] + faces[0][3], faces[0][0]:faces[0][0] + faces[0][2]:1, :]
+        frame = frame[faces[0][1]:faces[0][1] + faces[0][3], faces[0][0]:faces[0][0] + faces[0][2]:1]
+        eyes = eyeCascade.detectMultiScale(
+          frame,
+          scaleFactor=1.1,
+          minNeighbors=5,
+          minSize=(30, 30),
+          # flags = cv2.CV_HAAR_SCALE_IMAGE
         )
-        # print("Found {0} faces!".format(len(faces)))
-        if len(faces) > 0:
-            # Draw a rectangle around the faces
-            for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            frame_tmp = img[faces[0][1]:faces[0][1] + faces[0][3], faces[0][0]:faces[0][0] + faces[0][2]:1, :]
-            frame = frame[faces[0][1]:faces[0][1] + faces[0][3], faces[0][0]:faces[0][0] + faces[0][2]:1]
-            eyes = eyeCascade.detectMultiScale(
-                frame,
-                scaleFactor=1.1,
-                minNeighbors=5,
-                minSize=(30, 30),
-                # flags = cv2.CV_HAAR_SCALE_IMAGE
-            )
-            if len(eyes) == 0:
-                print('no eyes!!!')
-                num += 1
-            else:
-                print('eyes!!!')
-            frame_tmp = cv2.resize(frame_tmp, (400, 400), interpolation=cv2.INTER_LINEAR)
-            cv2.imshow('Face Recognition', frame_tmp)
-            cv2.imwrite(save_path + str(num) + ".jpg", frame_tmp)
-        waitkey = cv2.waitKey(1)
-        if waitkey == ord('q') or waitkey == ord('Q'):
-            cv2.destroyAllWindows()
-            break
+        # 目を閉じているか判定
+        if len(eyes) == 0:
+          print('no eyes!!!')
+          cnt += 1
+        else:
+          print('eyes!!!')
+          cnt = 0
+        frame_tmp = cv2.resize(frame_tmp, (400, 400), interpolation=cv2.INTER_LINEAR)
+        # 目を5秒以上閉じていると画像を保存
+        if cnt > 5:
+          print('sleep!!')
+          cv2.imwrite(save_path + str(num) + ".jpg", frame_tmp)
+          num += 1
+        cv2.imshow('Face Recognition', frame_tmp)
+    waitkey = cv2.waitKey(1)
+    if waitkey == ord('q') or waitkey == ord('Q'):
+      cv2.destroyAllWindows()
+      break
+  time.sleep(1)
+      
